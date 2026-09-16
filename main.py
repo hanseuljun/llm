c58328f89f10dc4e445e927acc175475c7c525d3
Hanseul Jun <hanseuljun@gmail.com>
2026-09-15 21:36:22 -0700

    map to vocabs

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 with open("data/held_out.txt") as held_out_file:
     held_out_lines = held_out_file.readlines()
 
@@ -5,8 +7,12 @@ with open("data/train.txt") as train_file:
     train_lines = train_file.readlines()
 
 with open("data/vocab.json") as vocab_file:
-    vocab = vocab_file.read()
+    vocab = json.load(vocab_file)
 
-print(held_out_lines)
-print(train_lines)
+train_line = train_lines[0]
+train_words = train_line.split()
+train_indices = [vocab[word] for word in train_words]
+
 print(vocab)
+print(train_words)
+print(train_indices)
