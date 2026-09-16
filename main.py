919ed33fd12e663ed7c78dfac823c6b36f316fbc
Hanseul Jun <hanseuljun@gmail.com>
2026-09-15 22:17:14 -0700

    train_lines_indices

--- main.py
+++ main.py
@@ -17,22 +17,23 @@ with open("data/train.txt") as train_file:
 with open("data/vocab.json") as vocab_file:
     vocab = json.load(vocab_file)
 
-train_line = train_lines[0]
-train_indices = convert_line_to_indices(train_line, vocab=vocab)
+train_lines_indices = [convert_line_to_indices(line, vocab=vocab) for line in train_lines]
+first_train_line_indices = train_lines_indices[0]
 
 inv_vocab = {value: key for key, value in vocab.items()}
-train_words = convert_indices_to_words(train_indices, inv_vocab=inv_vocab)
+first_train_line_words = convert_indices_to_words(first_train_line_indices, inv_vocab=inv_vocab)
 
 vocab_count = len(vocab)
 freq_table = {i: [0] * vocab_count for i in range(vocab_count)}
 
-for i in range(len(train_indices) - 1):
-    prev = train_indices[i]
-    next = train_indices[i+1]
-    freq_table[prev][next] += 1
+for indices in train_lines_indices:
+    for i in range(len(indices) - 1):
+        prev = indices[i]
+        next = indices[i+1]
+        freq_table[prev][next] += 1
 
 print(vocab)
-print(train_indices)
+print(first_train_line_indices)
 print(inv_vocab)
-print(train_words)
+print(first_train_line_words)
 print(freq_table)
