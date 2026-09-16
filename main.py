fa9886de0d121d3f37aa4b09f2a8d970167b9a88
Hanseul Jun <hanseuljun@gmail.com>
2026-09-15 22:06:04 -0700

    convert_indices_to_words

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import json
 
 
-def line_to_indices(line, vocab):
+def convert_line_to_indices(line: str, vocab: dict[str, int]) -> list[int]:
     words = ["<bos>"] + line.split() + ["<eos>"]
     return [vocab[word] for word in words]
+
+def convert_indices_to_words(indices: list[int], inv_vocab: dict[int, str]) -> list[str]:
+    return [inv_vocab[index] for index in indices]
 
 with open("data/held_out.txt") as held_out_file:
     held_out_lines = held_out_file.readlines()
@@ -15,10 +18,12 @@ with open("data/vocab.json") as vocab_file:
     vocab = json.load(vocab_file)
 
 train_line = train_lines[0]
-# train_words = ["<bos>"] + train_line.split() + ["<eos>"]
-# train_indices = [vocab[word] for word in train_words]
-train_indices = line_to_indices(train_line, vocab=vocab)
+train_indices = convert_line_to_indices(train_line, vocab=vocab)
+
+inv_vocab = {value: key for key, value in vocab.items()}
+train_words = convert_indices_to_words(train_indices, inv_vocab=inv_vocab)
 
 print(vocab)
-# print(train_words)
 print(train_indices)
+print(inv_vocab)
+print(train_words)
