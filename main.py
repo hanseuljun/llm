ad1d6d8408516aeda513046813ca03f55ffd25f8
Hanseul Jun <hanseuljun@gmail.com>
2026-09-15 21:31:55 -0700

    read files

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("llm")
+with open("data/held_out.txt") as held_out_file:
+    held_out_lines = held_out_file.readlines()
+
+with open("data/train.txt") as train_file:
+    train_lines = train_file.readlines()
+
+with open("data/vocab.json") as vocab_file:
+    vocab = vocab_file.read()
+
+print(held_out_lines)
+print(train_lines)
+print(vocab)
