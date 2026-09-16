a56ec26d1f01252752ca52cd04e92711528d8674
Hanseul Jun <hanseuljun@gmail.com>
2026-09-15 22:33:53 -0700

    def main()

--- main.py
+++ main.py
@@ -8,44 +8,47 @@ def convert_line_to_indices(line: str, vocab: dict[str, int]) -> list[int]:
 def convert_indices_to_words(indices: list[int], inv_vocab: dict[int, str]) -> list[str]:
     return [inv_vocab[index] for index in indices]
 
-with open("data/held_out.txt") as held_out_file:
-    held_out_lines = held_out_file.readlines()
+def main():
+    # with open("data/held_out.txt") as held_out_file:
+        # held_out_lines = held_out_file.readlines()
 
-with open("data/train.txt") as train_file:
-    train_lines = train_file.readlines()
+    with open("data/train.txt") as train_file:
+        train_lines = train_file.readlines()
 
-with open("data/vocab.json") as vocab_file:
-    vocab = json.load(vocab_file)
+    with open("data/vocab.json") as vocab_file:
+        vocab = json.load(vocab_file)
 
-train_lines_indices = [convert_line_to_indices(line, vocab=vocab) for line in train_lines]
-first_train_line_indices = train_lines_indices[0]
+    train_lines_indices = [convert_line_to_indices(line, vocab=vocab) for line in train_lines]
+    first_train_line_indices = train_lines_indices[0]
 
-inv_vocab = {value: key for key, value in vocab.items()}
-first_train_line_words = convert_indices_to_words(first_train_line_indices, inv_vocab=inv_vocab)
+    inv_vocab = {value: key for key, value in vocab.items()}
+    first_train_line_words = convert_indices_to_words(first_train_line_indices, inv_vocab=inv_vocab)
 
-vocab_count = len(vocab)
-freq_table = {i: [0] * vocab_count for i in range(vocab_count)}
+    vocab_count = len(vocab)
+    freq_table = {i: [0] * vocab_count for i in range(vocab_count)}
 
-for indices in train_lines_indices:
-    for i in range(len(indices) - 1):
-        prev = indices[i]
-        next = indices[i+1]
-        freq_table[prev][next] += 1
+    for indices in train_lines_indices:
+        for i in range(len(indices) - 1):
+            prev = indices[i]
+            next = indices[i+1]
+            freq_table[prev][next] += 1
 
-next_index_table = {key: value.index(max(value)) for key, value in freq_table.items()}
-pred_indices = [0]
-for _ in range(6):
-    next_index = next_index_table[pred_indices[-1]]
-    pred_indices.append(next_index)
-    if next_index == vocab["<eos>"]:
-        break
-pred_words = convert_indices_to_words(pred_indices, inv_vocab=inv_vocab)
+    next_index_table = {key: value.index(max(value)) for key, value in freq_table.items()}
+    pred_indices = [0]
+    for _ in range(6):
+        next_index = next_index_table[pred_indices[-1]]
+        pred_indices.append(next_index)
+        if next_index == vocab["<eos>"]:
+            break
+    pred_words = convert_indices_to_words(pred_indices, inv_vocab=inv_vocab)
 
-print(vocab)
-print(first_train_line_indices)
-print(inv_vocab)
-print(first_train_line_words)
-print(freq_table)
-print(next_index_table)
-print(pred_indices)
-print(pred_words)
+    print(vocab)
+    print(first_train_line_indices)
+    print(inv_vocab)
+    print(first_train_line_words)
+    print(freq_table)
+    print(next_index_table)
+    print(pred_indices)
+    print(pred_words)
+
+main()
