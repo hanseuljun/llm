b26082fd7bd75112b28a85720dafc94894d7ef21
Hanseul Jun <hanseuljun@gmail.com>
2026-09-17 22:11:27 -0700

    update weight

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ def run_bow():
         vocab = json.load(vocab_file)
 
     train_lines_indices = [convert_line_to_indices(line, vocab=vocab) for line in train_lines]
-    train_lines_indices = train_lines_indices[:2]
+    train_lines_indices = train_lines_indices[:100]
 
     train_pairs = []
     for train_line_indices in train_lines_indices:
@@ -147,19 +147,37 @@ def run_bow():
 
     mx.random.seed(0)
     model = nn.Linear(len(vocab), len(vocab))
+    optimizer = optimizers.SGD(0.05)
+
+    correct_count = 0
+    incorrect_count = 0
 
     for pair in train_pairs:
         gt_index = pair[1]
         output = model(pair[0])
         output_index = output.argmax()
+        target = mx.zeros(len(vocab))
+        target[gt_index] = 1
+        grads = mx.grad(nn.losses.cross_entropy)(output, target)
+        grads_mat = mx.zeros((len(vocab), len(vocab)))
+        for i in range(len(pair[0])):
+            grads_mat[:, i] = grads * pair[0][i]
+        optimizer.update(model, {"weight": grads_mat})
+
+        if output_index == gt_index:
+            correct_count += 1
+        else:
+            incorrect_count += 1
         print(f"gt_index: {gt_index}")
         print(f"output: {output}")
         print(f"output[16]: {output[16]}")
         print(f"output_index: {output_index}")
+        print(f"grads: {grads}")
 
     # print(train_line_indices)
     # print(train_line_word_embeds)
     # print(train_line_bow_embeds)
+    print(f"correct_count: {correct_count}, incorrect_count: {incorrect_count}")
 
 def main():
     # run_linear()
