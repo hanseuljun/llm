c0547c72a3c236efa4ad38b8e692d7691b9f1722
Hanseul Jun <hanseuljun@gmail.com>
2026-09-20 21:36:19 -0700

    skip turning target a one-hot

--- main.py
+++ main.py
@@ -89,12 +89,12 @@ def run_bow():
     BATCH_SIZE = 16
     for batch_token_id in range(len(train_qa_pairs) // BATCH_SIZE):
         batch_start_token_id = batch_token_id * BATCH_SIZE
-        for i in range(BATCH_SIZE):
-            pair = train_qa_pairs[batch_start_token_id + i]
-            target_token_id = pair[1]
-            target = mx.zeros(len(vocab))
-            target[target_token_id] = 1
-            _, grads = loss_and_grad_fn(pair[0], target)
+        pairs = train_qa_pairs[batch_start_token_id:batch_start_token_id+BATCH_SIZE]
+        inputs = [pair[0] for pair in pairs]
+        targets = [pair[1] for pair in pairs]
+        for input, target in zip(inputs, targets):
+            target = mx.array(target)
+            _, grads = loss_and_grad_fn(input, target)
             optimizer.update(model, grads)
             mx.eval(model.parameters(), optimizer.state)
 
