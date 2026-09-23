b102e3bf90116aa3860a68b9fe6cb686b173cf3d
Hanseul Jun <hanseuljun@gmail.com>
2026-09-23 00:44:47 -0700

    mask inside the loop

--- main.py
+++ main.py
@@ -42,7 +42,9 @@ class BOWModel(nn.Module):
             padded_token_ids = mx.pad(token_ids, (0, self.context_length - len(token_ids)))
             word_embed = self.word_embedding(padded_token_ids)
             position_embed = self.position_embedding(mx.arange(self.context_length))
-            bow_embed = mx.sum((word_embed * position_embed)[0:len(token_ids)], axis=0) / len(token_ids)
+            mask = mx.arange(self.context_length) < len(token_ids)
+            mask = mask.astype(mx.float32)
+            bow_embed = mx.sum(word_embed * position_embed * mask[:, None], axis=0) / len(token_ids)
             embeds.append(bow_embed)
         x = mx.stack(embeds, axis=0)
         for layer in self.layers[:-1]:
